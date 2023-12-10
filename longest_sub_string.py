c995c02cadb28b7a92dfc9f3a057a9c4521783a7
radium <trungtk.radium@gmail.com>
2023-12-10 10:57:53 +0700

    Minor fix

--- longest_sub_string.py
+++ longest_sub_string.py
@@ -1,9 +1,9 @@
 def longest_sub_string(s1, s2):
     if len(s1) > len(s2):
         s1, s2 = s2, s1
-    num = len(s1) - 1
+    num = len(s1)
     while num != 0:
-        for i in range(0, len(s1) - num):
+        for i in range(0, len(s1) - num + 1):
             if s1[i: i+num] in s2:
                 return num
         num -= 1
